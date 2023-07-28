611c5d1deea06a9ba4f76fc8a5721ef90698a781
Xin Yi Li <61605285+lixin-source@users.noreply.github.com>
2023-07-28 13:47:37 -0400

    Step 2

---
+++ creatorverse/src/components/Creator.jsx
@@ -0,0 +1,14 @@
+import '../App.css'
+
+
+const Creator = (name, url, description) =>  {
+
+
+  return (
+      <div className="Creator">
+          
+      </div>
+  );
+};
+
+export default Creator;
